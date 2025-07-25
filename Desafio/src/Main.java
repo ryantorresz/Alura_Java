dbd32368a4dd52bdcda7e5a623fbcdf00c081d43
Ryan Torres <ryantorres77690@gmail.com>
2025-07-25 16:14:47 -0300

    praticando desafios

--- Desafio/src/Main.java
+++ Desafio/src/Main.java
@@ -1,14 +1,17 @@
-public class Main {
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println(ehPalindromo("socorram me subi no onibus em marrocos")); // Saída: true
-        System.out.println(ehPalindromo("Java")); // Saída: false
-
-
-
-        }
-            public static boolean ehPalindromo(String palavra) {
-        String semEspacos = palavra.replace("", "").toLowerCase();
-        return new StringBuilder(semEspacos).reverse().toString().equalsIgnoreCase(semEspacos);
-
-        }
+        List<String> emails = Arrays.asList("TESTE@EXEMPLO.COM", "exemplo@Java.com ", "Usuario@teste.Com");
+        System.out.println(converterEmails(emails));
+        // Saída: ["teste@exemplo.com", "exemplo@java.com", "usuario@teste.com"]
     }
+    public static List<String> converterEmails(List<String> emails) {
+        return emails.stream()
+                .map(email -> email.trim().toLowerCase())
+                .toList();
+    }
+}

23032bbcfb8af6d56a4bf3b158daecbe421bd544
Ryan Torres <ryantorres77690@gmail.com>
2025-07-25 16:07:00 -0300

    desafios alura

--- Desafio/src/Main.java
+++ Desafio/src/Main.java
@@ -1,19 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(obterPrimeiroEUltimoNome("  João Carlos Silva   ")); // Saída: "João Silva"
-        System.out.println(obterPrimeiroEUltimoNome("Maria   ")); // Saída: "Maria"
-    }
+        System.out.println(ehPalindromo("socorram me subi no onibus em marrocos")); // Saída: true
+        System.out.println(ehPalindromo("Java")); // Saída: false
 
-    public static String obterPrimeiroEUltimoNome(String nomeCompleto) {
-        // Remove espaços extras e divide em partes
-        String[] partes = nomeCompleto.trim().split("\\s+");
 
-        // Se só tiver um nome, retorna ele mesmo
-        if (partes.length == 1) {
-            return partes[0];
+
         }
+            public static boolean ehPalindromo(String palavra) {
+        String semEspacos = palavra.replace("", "").toLowerCase();
+        return new StringBuilder(semEspacos).reverse().toString().equalsIgnoreCase(semEspacos);
 
-        // Combina primeiro e último nome
-        return partes[0] + " " + partes[partes.length - 1];
+        }
     }
-}
